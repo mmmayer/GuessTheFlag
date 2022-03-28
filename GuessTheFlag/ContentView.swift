910c6d5c4d547a523b676e9afe6963e62378f12e
Michael M. Mayer <m.mayer6@me.com>
2022-03-27 19:53:48 -0500

    1) Added flag emoji to the error message when a wrong flag is selected. 2) Added an animation when the correct flag is selected.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,7 +11,9 @@ import SwiftUI
 // A list of countries that have flag assets in the app
 var countries = ["China", "Iceland", "Mali", "Spain", "Algeria", "Columbia", "Indonesia", "Mauritius", "Sudan", "Ireland", "Switzerland", "Armenia", "Cuba", "Israel", "Morocco", "Syria", "Australia", "Denmark", "Italy", "Nigeria", "Thailand", "Austria", "Estonia", "Jamaica", "Norway", "Turkey", "Bangladesh", "Finland", "Japan", "Poland", "UK", "Belgium", "Kuwait", "Republic of the Congo", "US", "Bolivia", "France", "Laos", "Romania", "Ukraine", "Botswana", "Germany", "Latvia", "Russia", "Vietnam", "Bulgaria", "Greece", "Liberia", "Sierra Leone", "Yemen", "Canada", "Guinea", "Lithuania", "Singapore", "Guyana", "Somalia", "Chile", "Hungary", "Madagascar", "South Africa"].shuffled()
 
-// A modifier for flag umages
+var countryFlags = ["China" : "🇨🇳", "Iceland" : "🇮🇸", "Mali" : "🇲🇱", "Spain" : "🇪🇸", "Algeria" : "🇩🇿", "Columbia" : "🇨🇴", "Indonesia" : "🇮🇩", "Mauritius" : "🇲🇺", "Sudan" : "🇸🇩", "Ireland" : "🇮🇪", "Switzerland" : "🇨🇭", "Armenia" : "🇦🇲", "Cuba" : "🇨🇺", "Israel" : "🇮🇱", "Morocco" : "🇲🇦", "Syria" : "🇸🇾", "Australia" : "🇦🇺", "Denmark" : "🇩🇰", "Italy" : "🇮🇹", "Nigeria" : "🇳🇬", "Thailand" : "🇹🇭", "Austria" : "🇦🇹", "Estonia" : "🇪🇪", "Jamaica" : "🇯🇲", "Norway" : "🇳🇴", "Turkey" : "🇹🇷", "Bangladesh" : "🇧🇩", "Finland" : "🇫🇮", "Japan" : "🇯🇵", "Poland" : "🇵🇱", "UK" : "🇬🇧", "Belgium" : "🇧🇪", "Kuwait" : "🇰🇼", "Republic of the Congo" : "🇨🇬", "US" : "🇺🇸", "Bolivia" : "🇧🇴", "France" : "🇫🇷", "Laos" : "🇱🇦", "Romania" : "🇷🇴", "Ukraine" : "🇺🇦", "Botswana" : "🇧🇼", "Germany" : "🇩🇪", "Latvia" : "🇱🇻", "Russia" : "🇷🇺", "Vietnam" : "🇻🇳", "Bulgaria" : "🇧🇬", "Greece" : "🇬🇷", "Liberia" : "🇱🇷", "Sierra Leone" : "🇸🇱", "Yemen" : "🇾🇪", "Canada" : "🇨🇦", "Guinea" : "🇬🇳", "Lithuania" : "🇱🇹", "Singapore" : "🇸🇬", "Guyana" : "🇬🇾", "Somalia" : "🇸🇴", "Chile" : "🇨🇱", "Hungary" : "🇭🇺", "Madagascar" : "🇲🇬", "South Africa": "🇿🇦"]
+
+// A modifier for flag images
 struct FlagImage: ViewModifier {
     func body(content: Content) -> some View {
         content
@@ -32,10 +34,11 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var questionCount = 0
     @State private var isScoreShowing = false
+    @State private var animationAmount: CGFloat = 0.0
     
     @State private var score = 0
     @State private var countrySelected = ""
-        
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -52,12 +55,18 @@ struct ContentView: View {
                     
                     ForEach(0..<3) { number in
                         Button {
+                            withAnimation(.linear) {
+                                if number == correctAnswer {
+                                    animationAmount += 720.0
+                                }
+                            }
                             flagTapped(num: number)
                         } label: {
                             Image(countries[number])
                                 .renderingMode(.original)
                                 .flagStyle()
                         }
+                        .rotation3DEffect(.degrees(number == correctAnswer ? animationAmount : 0), axis: (x: 1, y: 0, z: 0))
                     }
                     Spacer()
                     VStack(spacing: 10) {
@@ -81,7 +90,8 @@ struct ContentView: View {
                 }
             }
             .alert(isPresented: $isScoreShowing) {
-                Alert(title: Text("Incorrect"), message: Text("You chose the flag for \(countrySelected)"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text("Incorrect"), message: Text("You chose the flag for \(countrySelected) \(countryFlags[countrySelected]!)."), dismissButton: .default(Text("Continue")) {
+                    newQuestion()
                 })
             }
         }
@@ -90,15 +100,16 @@ struct ContentView: View {
     // These are the actions taken when a flag is tapped.
     // A new score is calculated and we set up for the next round
     func flagTapped(num: Int) {
+        animationAmount = 0.0
         questionCount += 1
         if num == correctAnswer {
             score += 1
+            newQuestion()
         }
         else {
             isScoreShowing = true
             countrySelected = countries[num]
         }
-        newQuestion()
     }
     
     // Sets up the new round
@@ -109,6 +120,7 @@ struct ContentView: View {
     
     // Starts a new round in a new game by shuffling and resetting values
     func reset() {
+        animationAmount = 0.0
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         questionCount = 0
